21950940a895181a292d1f955c2e8a3008ea2c8d
mkuek <mkuek123@gmail.com>
2021-11-27 12:16:45 -0500

    Fixed stand button
    Stand button now gives card to dealer until point value >= 17

--- blackjack-exercise/scripts/main.js
+++ blackjack-exercise/scripts/main.js
@@ -29,10 +29,16 @@ window.addEventListener("DOMContentLoaded", function () {
   let deal = document.getElementById("deal-button");
   deal.addEventListener("click", dealCards);
   function dealCards() {
-    dealDeck("player", 2);
-    render("player");
-    dealDeck("dealer", 2);
-    render("dealer");
+    let dealerPoints = document.getElementById("dealer-points").innerText;
+    let playerPoints = document.getElementById("player-points").innerText;
+    let messages = document.getElementById("messages");
+    dealDeck("players", 2);
+    document.getElementById("deal-button").disabled = true;
+    document.getElementById("hit-button").disabled = false;
+    document.getElementById("stand-button").disabled = false;
+    // render("player", 2);
+    // dealDeck("dealer", 1);
+    // render("dealer", 2);
     // for (let i = 0; i < 4; i++) {
     //   let dealerTarget = document.getElementById("dealer-hand");
     //   let dealerHand = document.createElement("img");
@@ -48,10 +54,13 @@ window.addEventListener("DOMContentLoaded", function () {
   let hit = document.getElementById("hit-button");
   hit.addEventListener("click", hitMe);
   function hitMe() {
+    let dealerPoints = document.getElementById("dealer-points").innerText;
+    let playerPoints = document.getElementById("player-points").innerText;
+    let messages = document.getElementById("messages");
+    if (dealerPoints <= 17) {
+      dealDeck("dealer", 1);
+    }
     dealDeck("player", 1);
-    render("player");
-    dealDeck("dealer", 1);
-    render("dealer");
 
     // let playerTarget = document.getElementById("player-hand");
     // let playerHand = document.createElement("img");
@@ -65,43 +74,125 @@ window.addEventListener("DOMContentLoaded", function () {
 
   let stand = document.getElementById("stand-button");
   stand.addEventListener("click", () => {
-    dealDeck("player", 3);
-    render("player");
-    dealDeck("dealer", 3);
-    render("dealer");
+    let dealerPoints = document.getElementById("dealer-points").innerText;
+    let playerPoints = document.getElementById("player-points").innerText;
+    let messages = document.getElementById("messages");
+    console.log("dealer points:" + dealerPoints);
+    console.log("player points:" + playerPoints);
+    console.log(dealerPoints > playerPoints);
+    let i = 0;
+
+    while (dealerPoints < 17) {
+      dealDeck("dealer", 1);
+      dealerPoints = document.getElementById("dealer-points").innerText;
+      console.log(dealerPoints);
+      if (dealerPoints >= 17 && dealerPoints <= 21) {
+        if (dealerPoints > playerPoints && dealerPoints <= 21) {
+          messages.innerText = "DEALER WINS";
+          return alert("DEALER WINS");
+        } else if (playerPoints > dealerPoints) {
+          return (messages.innerText = "PLAYER WINS");
+        }
+      } else if (playerPoints > dealerPoints || dealerPoints > 21) {
+        return (messages.innerText = "PLAYER WINS");
+      }
+      i++;
+    }
+    if (dealerPoints >= 17 && dealerPoints <= 21) {
+      if (dealerPoints > playerPoints && dealerPoints <= 21) {
+        messages.innerText = "DEALER WINS";
+        return alert("DEALER WINS");
+      }
+    }
+
+    // else if (dealerPoints <= 17) {
+    //   dealDeck("dealer", 1);
+    // }
   });
-  // var deck = buildDeck();
+  var deck = buildDeck();
   // var hands = [];
   // var dealerHand = [];
   // var playerHand = [];
   // var card = [];
-  function dealDeck(player, numCards) {
+  function dealDeck(target, numCards) {
     let dealerHand = [];
     let playerHand = [];
     let hands = [];
-    let deck = buildDeck();
-    for (let i = 0; i < numCards; i++) {
-      for (let player = 0; player < 2; player++) {
+    shuffle(deck);
+    // let deck = buildDeck();
+    for (let i = 0; i <= numCards - 1; i++) {
+      if (target == "players") {
+        for (let player = 0; player < 2; player++) {
+          let card = deck.pop();
+          hands.push(card);
+        }
+      } else {
         let card = deck.pop();
-
         hands.push(card);
       }
     }
-
-    // for (let i = 0; i < hands.length; i += 2) {
-    //   dealerHand.push(hands[i]);
-    //   playerHand.push(hands[i + 1]);
-    // }
-
-    dealerHand.push(hands[0]);
-    dealerHand.push(hands[2]);
-    playerHand.push(hands[1]);
-    playerHand.push(hands[3]);
-    if (player == "dealer") {
-      return dealerHand;
-    } else if (player == "player") {
-      return playerHand;
+    if (target == "players") {
+      for (let i = 0; i <= hands.length - 1; i += 2) {
+        dealerHand.push(hands[i]);
+        playerHand.push(hands[i + 1]);
+      }
+    } else if (target == "player") {
+      for (let i = 0; i <= hands.length - 1; i += 2) {
+        playerHand.push(hands[i]);
+      }
+    } else if (target == "dealer") {
+      for (let i = 0; i <= hands.length - 1; i += 2) {
+        dealerHand.push(hands[i]);
+      }
     }
+
+    // dealerHand.push(hands[0]);
+    // dealerHand.push(hands[2]);
+    // playerHand.push(hands[1]);
+    // playerHand.push(hands[3]);
+    // console.log(playerHand);
+    // console.log(dealerHand);
+
+    let playerPoints = document.getElementById("player-points");
+    render("player", playerHand.length, playerHand);
+    if (playerPoints.innerText > 21) {
+      return (messages.innerText = "PLAYER BUSTED");
+    }
+    render("dealer", dealerHand.length, dealerHand);
+
+    // function sleep(ms) {
+    //   return new Promise((resolve) => setTimeout(resolve, ms));
+    // }
+    // async function demo() {
+    //   let playerPoints = document.getElementById("player-points");
+    //   render("player", playerHand.length, playerHand);
+    //   if (playerPoints.innerText > 21) {
+    //     return (messages.innerText = "PLAYER BUSTED");
+    //   }
+    //   await sleep(0);
+    //   render("dealer", dealerHand.length, dealerHand);
+    // }
+    // demo();
+
+    // for (let i = 0; i <= dealerHand.length - 1; i++) {
+    //   let dealerTarget = document.getElementById("dealer-hand");
+    //   let dealerCards = document.createElement("img");
+    //   dealerCards.src = getcardimage(dealerHand[i]);
+    //   dealerTarget.appendChild(dealerCards);
+    // }
+    // for (let i = 0; i <= playerHand.length - 1; i++) {
+    //   let playerTarget = document.getElementById("player-hand");
+    //   let playerCards = document.createElement("img");
+    //   playerCards.src = getcardimage(playerHand[i]);
+    //   playerTarget.appendChild(playerCards);
+    // }
+    // if (player == "dealer") {
+    //   console.log("dealer Hand :" + dealerHand);
+
+    //   return dealerHand;
+    // } else if (player == "player") {
+    //   return playerHand;
+    // }
 
     // console.log(JSON.stringify(dealerHand));
     // dealerHand.forEach((card) => {
@@ -114,25 +205,79 @@ window.addEventListener("DOMContentLoaded", function () {
   function getcardimage(card) {
     return `images/${card.Rank}_of_${card.suit}.png`;
   }
-  function render(target) {
-    let playerStart = dealDeck("player", 2);
-    let dealerStart = dealDeck("dealer", 2);
+  function render(target, num, hand) {
+    let messages = document.getElementById("messages");
     if (target == "dealer") {
-      // console.log("dealer hand: " + dealerHand);
-      for (let i = 0; i < 2; i++) {
+      calculatePoints(hand);
+      for (let i = 0; i < num; i++) {
         let dealerTarget = document.getElementById("dealer-hand");
-        let dealerHand = document.createElement("img");
-        dealerHand.src = getcardimage(dealerStart[i]);
-        dealerTarget.appendChild(dealerHand);
+        let dealerCards = document.createElement("img");
+        let dealerPoints = document.getElementById("dealer-points");
+        let playerPoints = document.getElementById("player-points");
+        dealerCards.src = getcardimage(hand[i]);
+        dealerTarget.appendChild(dealerCards);
+        dealerPoints.innerText = handPoints - playerPoints.innerText;
+        if (playerPoints.innerText > 21) {
+          return console.log("player busted");
+        } else if (dealerPoints.innerText > 21) {
+          document.getElementById("hit-button").disabled = true;
+          document.getElementById("stand-button").disabled = true;
+          return (messages.innerText = "DEALER BUSTED");
+        } else if (dealerPoints.innerText == 21) {
+          document.getElementById("hit-button").disabled = true;
+          document.getElementById("stand-button").disabled = true;
+          return (messages.innerText = "DEALER WINS");
+        }
       }
     } else if (target == "player") {
-      // console.log("player hand: " + playerHand);
-      for (let i = 0; i < 2; i++) {
+      calculatePoints(hand);
+      for (let i = 0; i < num; i++) {
         let playerTarget = document.getElementById("player-hand");
-        let playerHand = document.createElement("img");
-        playerHand.src = getcardimage(playerStart[i]);
-        playerTarget.appendChild(playerHand);
+        let playerCards = document.createElement("img");
+        let dealerPoints = document.getElementById("dealer-points");
+        let playerPoints = document.getElementById("player-points");
+        playerCards.src = getcardimage(hand[i]);
+        playerTarget.appendChild(playerCards);
+        playerPoints.innerText = handPoints - dealerPoints.innerText;
+        if (playerPoints.innerText > 21) {
+          document.getElementById("hit-button").disabled = true;
+          document.getElementById("stand-button").disabled = true;
+          return (messages.innerText = "PLAYER BUSTED");
+        }
       }
     }
+  }
+
+  function shuffle(deck) {
+    for (let i = 0; i < 500; i++) {
+      let swap1 = Math.floor(Math.random() * deck.length);
+      // console.log(swap1);
+      let swap2 = Math.floor(Math.random() * deck.length);
+      // console.log(swap2);
+      let temp = deck[swap1];
+      deck[swap1] = deck[swap2];
+      deck[swap2] = temp;
+    }
+    return deck;
+  }
+  var handPoints = 0;
+
+  function calculatePoints(hand) {
+    for (i = 0; i < hand.length; i++) {
+      // console.log(typeof parseInt(hand[i].Rank));
+      // console.log(typeof handPoints);
+      // if (hand[i].Rank == "ace" && isNaN(parseInt(hand[i + 1].Rank)) == true) {
+      //   handPoints += 21;
+      // }
+      if (hand[i].Rank == "ace") {
+        handPoints += 1;
+      } else if (isNaN(parseInt(hand[i].Rank)) == true) {
+        // console.log("Hello");
+        handPoints += 10;
+      } else {
+        handPoints += parseInt(hand[i].Rank);
+      }
+    }
+    // console.log(handPoints);
   }
 });
