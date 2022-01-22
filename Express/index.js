fc361edc7ec11c1a01e29a92cb67e50182e6ea52
Michael Kuek <mkuek123@gmail.com>
2022-01-21 21:27:54 -0500

    First express project

---
+++ Express/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+// app.use((req, res) => {
+//   console.log("WE GOT A REQUEST");
+//   console.dir(req);
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is the homepage");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  //   console.log(req.params);
+  const { subreddit, postID } = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`);
+});
+
+app.get("/r/:subreddit/:postID", (req, res) => {
+  //   console.log(req.params);
+  const { subreddit, postID } = req.params;
+  res.send(
+    `<h1>Viewing the PostID: ${postID} on the ${subreddit} subreddit</h1>`
+  );
+});
+
+app.post("/cats", (req, res) => {
+  res.send("Post requrest to /cats! This is a post request");
+});
+app.get("/cats", (req, res) => {
+  res.send("MEOW!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!");
+});
+app.get("*", (req, res) => {
+  res.send("Cannot find page!");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
